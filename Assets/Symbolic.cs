a5f3a05b97c6dddc7fa0ec5393526602d46eb320
Zoe McCarthy <zoemccarthy12@gmail.com>
2014-11-10 23:56:09 -0600

    added initial exploration into symbolic code, does not work yet.

---
+++ Assets/Symbolic.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Linq.Expressions;
+//using System.Reflection.Emit;
+
+public class Symbolic : MonoBehaviour {
+
+	//Expression<Func<int, bool>> lambda = num => num < 5;
+
+	// Use this for initialization
+	void Start () {
+	
+		// Manually build the expression tree for  
+		// the lambda expression num => num < 5.
+		ParameterExpression numParam = Expression.Parameter(typeof(int), "num");
+		ConstantExpression five = Expression.Constant(5, typeof(int));
+		BinaryExpression numLessThanFive = Expression.LessThan(numParam, five);
+		//Expression<F
+		//Expression<Func<int, bool>> lambda1;// =
+		//	Expression.Lambda<Func<int, bool>>(
+		//		numLessThanFive,
+		//		new ParameterExpression[] { numParam });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
